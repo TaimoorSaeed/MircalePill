2df8f2ef5feb03d0db53596b3466b826835e4bc5
taimoor <taimoor_saeed@hotmail.com>
2016-10-16 06:27:34 +0500

    intial UI

---
+++ MircalePillTests/MircalePillTests.swift
@@ -0,0 +1,36 @@
+//
+//  MircalePillTests.swift
+//  MircalePillTests
+//
+//  Created by Taimoor Saeed  on 16/10/2016.
+//  Copyright © 2016 Taimoor Saeed . All rights reserved.
+//
+
+import XCTest
+@testable import MircalePill
+
+class MircalePillTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
